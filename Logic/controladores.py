589002bdc84bfdac0ac7d842502105b81efe08b2
Gregory <glrd4726ml@gmail.com>
2025-11-19 21:02:19 -0300

    Utilizando Colorama e implementacion de virtualenv

--- Logic/controladores.py
+++ Logic/controladores.py
@@ -2,6 +2,7 @@ import os, time, sqlite3
 from Logic.persistencia import *
 from colorama import init, Fore, Back, Style
 
+init()
 def ingreso_usuario(usuario, password):
     try:
         conectar = sqlite3.connect("DB/productosapp.db")        
@@ -11,11 +12,11 @@ def ingreso_usuario(usuario, password):
         cursor.execute(sql, valor)
         resultado = cursor.fetchone()
         if resultado:
-            return "Ingreso exitoso"
+            return Fore.YELLOW+Style.BRIGHT+"Ingreso exitoso"+Style.RESET_ALL + Fore.RESET
         else:
             return False 
     except Exception as e:
-        print(f"Error al ingresar el usuario: {e}")
+        print(Fore.RED+Style.BRIGHT+f"Error al ingresar el usuario: {e}"+Style.RESET_ALL + Fore.RESET)
         return False
 
 def ver_usuarios():
@@ -24,11 +25,11 @@ def ver_usuarios():
         cursor = conectar.cursor()
         cursor.execute("SELECT * FROM usuarios WHERE estado=1")
         usuarios = cursor.fetchall()
-        print("Usuarios activos:")
+        print(Fore.YELLOW+"\nUsuarios activos:"+Fore.RESET)
         for usuario in usuarios:
             print(f"\nIndice: {usuario[0]}\nNombre: {usuario[1]}\nUsuario: {usuario[2]}\nEmail: {usuario[3]}\nRol: {usuario[5]}")
     except Exception as e:
-        print(f"Error al obtener los usuarios: {e}")
+        print(Fore.RED+"\nError al obtener los usuarios: {e}"+Fore.RESET)
     finally:
         conectar.close()
        
@@ -42,9 +43,9 @@ def crear_usuario(nombre, usuario, email, password, rol):
         valores = (nombre, usuario, email, password, rol)
         cursor.execute(sql, valores)
         conectar.commit()
-        print("Usuario creado con exito")
+        print(Fore.YELLOW+"\nUsuario creado con exito"+Fore.RESET)
     except Exception as e:
-        print(f"Error al crear el usuario: {e}")
+        print(Fore.RED+f"\nError al crear el usuario: {e}"+Fore.RESET)
     finally:
         conectar.close()
 
@@ -57,11 +58,11 @@ def modificar_usuario(id, nombre, usuario, email, password, rol):
         cursor.execute(sql, valores)
         conectar.commit()
         if cursor.rowcount > 0:
-            print("Usuario modificado con exito")
+            print(Fore.YELLOW+"\nUsuario modificado con exito"+Fore.RESET)
         else:
-            print("No se encontró el usuario con ese índice.")
+            print(Fore.RED+"\nNo se encontró el usuario con ese índice."+Fore.RESET)
     except Exception as e:
-        print(f"Error al modificar el usuario: {e}")
+        print(Fore.RED+f"\nError al modificar el usuario: {e}"+Fore.RESET)
     finally:
         conectar.close()
 
@@ -75,11 +76,11 @@ def eliminar_usuario(indice):
         cursor.execute(sql, valor)
         conectar.commit()
         if cursor.rowcount > 0:
-            print("Usuario eliminado con exito")
+            print(Fore.YELLOW+"\nUsuario eliminado con exito"+Fore.RESET)
         else:
-            print("No se encontró el usuario con ese índice.")
+            print(Fore.RED+"\nNo se encontró el usuario con ese índice."+Fore.RESET)
     except Exception as e:
-        print(f"Error al eliminar el usuario: {e}")
+        print(Fore.RED+f"\nError al eliminar el usuario: {e}"+Fore.RESET)
     finally:
         conectar.close()
         
@@ -93,9 +94,9 @@ def agregar_producto(nombre, precio, categoria, descripcion):
         valores = (nombre, precio, categoria, descripcion)
         cursor.execute(sql, valores)
         conectar.commit()
-        print("Producto agregado con exito")
+        print(Fore.YELLOW+"\nProducto agregado con exito"+Fore.RESET)
     except Exception as e:
-        print(f"Error al agregar el producto: {e}")
+        print(Fore.RED+f"\nError al agregar el producto: {e}"+Fore.RESET)
     finally:
         conectar.close()
 
@@ -105,11 +106,10 @@ def ver_productos():
         cursor = conectar.cursor()
         cursor.execute("SELECT * FROM productos WHERE estado=1")
         productos = cursor.fetchall()
-        print("Productos activos:")
         for producto in productos:
             print(f"\nIndice: {producto[0]}\nNombre: {producto[1]}\nPrecio: {producto[2]}\nCategoria: {producto[3]}\nDescripcion: {producto[4]}\nFecha Agregado: {producto[5]}")
     except Exception as e:
-        print(f"Error al obtener los productos: {e}")
+        print(Fore.RED+f"Error al obtener los productos: {e}"+Fore.RESET)
     finally:
         conectar.close() 
  
@@ -122,13 +122,13 @@ def buscar_producto(nombre):
         cursor.execute(sql, valor)
         productos = cursor.fetchall()
         if productos:
-            print("Productos encontrados:")
+            print(Fore.YELLOW+"Productos encontrados:"+Fore.RESET)
             for producto in productos:
                 print(f"\nIndice: {producto[0]}\nNombre: {producto[1]}\nPrecio: {producto[2]}\nCategoria: {producto[3]}\nDescripcion: {producto[4]}\nFecha Agregado: {producto[5]}")
         else:
-            print("No se encontraron productos con ese nombre.")
+            print(Fore.RED+"\nNo se encontraron productos con ese nombre."+Fore.RESET)
     except Exception as e:
-        print(f"Error al buscar el producto: {e}")
+        print(Fore.RED+f"\nError al buscar el producto: {e}"+Fore.RESET)
     finally:
         conectar.close()
 
@@ -141,11 +141,11 @@ def modificar_producto(id, nombre, precio, categoria, descripcion):
         cursor.execute(sql, valores)
         conectar.commit()
         if cursor.rowcount > 0:
-            print("Producto modificado con exito")
+            print(Fore.YELLOW+"\nProducto modificado con exito"+Fore.RESET)
         else:
-            print("No se encontró el producto con ese índice.")
+            print(Fore.RED+"\nNo se encontró el producto con ese índice."+Fore.RESET)
     except Exception as e:
-        print(f"Error al modificar el producto: {e}")
+        print(Fore.RED+f"\nError al modificar el producto: {e}"+Fore.RESET)
     finally:
         conectar.close()        
         
@@ -158,11 +158,11 @@ def eliminar_producto(indice):
         cursor.execute(sql, valor)
         conectar.commit()
         if cursor.rowcount > 0:
-            print("Producto eliminado con exito")
+            print(Fore.YELLOW+"\nProducto eliminado con exito"+Fore.RESET)
         else:
-            print("No se encontró el producto con ese índice.")
+            print(Fore.RED+"\nNo se encontró el producto con ese índice."+Fore.RESET)
     except Exception as e:
-        print(f"Error al eliminar el producto: {e}")
+        print(Fore.RED+f"Error al eliminar el producto: {e}"+Fore.RESET)
     finally:
         conectar.close()
 
--- Logic/logica.py
+++ Logic/logica.py
@@ -3,18 +3,18 @@ from colorama import init, Fore, Back
 from Logic.controladores import *
 from Logic.persistencia import *
 
-
+init()
 def ingreso():
   try:
     init()
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(Back.CYAN + "Bienvenido al sistema de gestión de productos\n" + Back.RESET)
+    print(Fore.YELLOW + Style.BRIGHT+ "Bienvenido al sistema de gestión de productos\n" + Style.RESET_ALL + Fore.RESET)
     usuario = input("Ingrese su usuario: ").strip().upper()
     password = input("Ingrese su contraseña: ").strip()
     
     if ingreso_usuario(usuario, password):
-      print(Fore.GREEN + "Ingreso exitoso\n"+Fore.RESET)
-      print("Cargando el sistema...")
+      print(Fore.YELLOW + "Ingreso exitoso\n"+Fore.RESET)
+      print(Fore.YELLOW+"Cargando el sistema..."+Fore.RESET)
       time.sleep(1)
       os.system('cls' if os.name == 'nt' else 'clear')
       ver_menu()
@@ -36,7 +36,7 @@ def ver_menu():
         cursor = conectar.cursor()
         cursor.execute("SELECT * FROM menu WHERE estado=1")
         menus = cursor.fetchall()
-        print(Back.CYAN+"Menu principal:\n"+Back.RESET)
+        print(Fore.YELLOW+Style.BRIGHT+"Menu principal:\n"+Fore.RESET+Style.RESET_ALL)
         for menu in menus:
             time.sleep(0.4)
             print(f"{menu[0]}.- {menu[1]}")
@@ -47,18 +47,18 @@ def ver_menu():
         conectar.close()
         
 def submenu():
-    seleccion = input(Fore.GREEN+"\nSeleccione una opción del menú principal: "+ Fore.RESET)
+    seleccion = input(Fore.YELLOW+Style.BRIGHT+"\nSeleccione una opción del menú principal: "+ Fore.RESET+Style.RESET_ALL)
     try:
         init()
         if seleccion == '1':
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(Back.CYAN+"Agregar nuevo producto:\n"+Back.RESET)
+            print(Fore.YELLOW+"Agregar nuevo producto:\n"+Fore.RESET)
             nombre = input("Ingrese el nombre del producto: ").strip().upper()
             precio = float(input("Ingrese el precio del producto: "))
             categoria = input("Ingrese la categoría del producto: ").strip().upper()
             descripcion = input("Ingrese la descripción del producto: ").strip()
             if nombre == "" or precio <= 0 or categoria == "":
-                print(Fore.RED + "Error: Todos los campos son obligatorios y el precio debe ser mayor a cero."+Fore.RESET)
+                print(Fore.RED + "\nError: Todos los campos son obligatorios y el precio debe ser mayor a cero."+Fore.RESET)
                 time.sleep(2)
                 os.system('cls' if os.name == 'nt' else 'clear')
                 ver_menu()
@@ -70,22 +70,22 @@ def submenu():
                 
         elif seleccion == '2':
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Ver productos:\n")
+            print(Fore.YELLOW+"Ver productos:\n"+Fore.RESET)
             ver_productos()
-            input(Fore.GREEN+"\nPresione Enter para volver al menú principal..."+Fore.RESET)
+            input(Fore.YELLOW+"\nPresione Enter para volver al menú principal..."+Fore.RESET)
             os.system('cls' if os.name == 'nt' else 'clear')
             ver_menu()
         elif seleccion == '3':
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(Back.CYAN+"Buscar producto:\n"+Back.RESET)
+            print(Fore.YELLOW+"Buscar producto:\n"+Fore.RESET)
             termino_busqueda = input("Ingrese el nombre del producto a buscar: ")
             buscar_producto(termino_busqueda)
-            input(Fore.GREEN+"\nPresione Enter para volver al menú principal..."+Fore.RESET)
+            input(Fore.YELLOW+"\nPresione Enter para volver al menú principal..."+Fore.RESET)
             os.system('cls' if os.name == 'nt' else 'clear')
             ver_menu()
         elif seleccion == '4':
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(Back.CYAN+"Modificar producto:\n"+Back.RESET)
+            print(Fore.YELLOW+"Modificar producto:\n"+Fore.RESET)
             id_producto = int(input("Ingrese el ID del producto a modificar: ")).strip()
             nuevo_nombre = input("Ingrese el nuevo nombre del producto: ").strip().upper()
             nuevo_precio = float(input("Ingrese el nuevo precio del producto: "))
@@ -104,7 +104,7 @@ def submenu():
                 ver_menu()
         elif seleccion == '5':
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(Back.CYAN+"Eliminar producto:\n"+Back.RESET)
+            print(Fore.YELLOW+"Eliminar producto:\n"+Fore.RESET)
             id_producto = int(input("Ingrese el ID del producto a eliminar: ")).strip()
             if not id_producto or id_producto <= 0 or id_producto == "" or id_producto is None:
                 print(Fore.RED + "Error: El ID del producto es obligatorio." + Fore.RESET)
@@ -118,19 +118,19 @@ def submenu():
                 ver_menu()
         elif seleccion == '6':
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(Back.CYAN+"Gestión de usuarios:\n"+Back.RESET)
+            print(Fore.YELLOW+"Gestión de usuarios:\n"+Fore.RESET)
             menu_usuarios = ["1.- Ver usuarios", "2.- Agregar usuario", "3.- Modificar usuario", "4.- Eliminar usuario"]
             for item in menu_usuarios:
                 print(item)
                 time.sleep(0.4)
-            opcion_usuario = input(Fore.GREEN+"\nSeleccione una opción del menú de usuarios: "+Fore.RESET)
+            opcion_usuario = input(Fore.YELLOW+Style.BRIGHT+"\nSeleccione una opción del menú de usuarios: "+Fore.RESET+Style.RESET_ALL)
             
             os.system('cls' if os.name == 'nt' else 'clear')
             if opcion_usuario == '1':
                 ver_usuarios()
-                input(Fore.GREEN+"\nPresione Enter para volver al menú principal..."+Fore.RESET)
+                input(Fore.YELLOW+"\nPresione Enter para volver al menú principal..."+Fore.RESET)
             elif opcion_usuario == '2':
-                print(Back.CYAN+"Agregar nuevo usuario:\n"+Back.RESET)
+                print(Fore.YELLOW+"Agregar nuevo usuario:\n"+Fore.RESET)
                 nombre = input("Ingrese el nombre completo: ").strip().upper()
                 usuario = input("Ingrese el nombre de usuario: ").strip().upper()
                 email = input("Ingrese el email: ").strip().lower()
@@ -147,7 +147,7 @@ def submenu():
                     time.sleep(2)
             
             elif opcion_usuario == '3':
-                print(Back.CYAN+"Modificar usuario:\n"+Back.RESET)
+                print(Fore.YELLOW+"Modificar usuario:\n"+Fore.RESET)
                 id_usuario = int(input("Ingrese el ID del usuario a modificar: ")).strip()
                 nuevo_nombre = input("Ingrese el nuevo nombre completo: ").strip().upper()
                 nuevo_usuario = input("Ingrese el nuevo nombre de usuario: ").strip().upper()
@@ -165,7 +165,7 @@ def submenu():
                     time.sleep(2)
             
             elif opcion_usuario == '4':
-                print(Back.CYAN+"Eliminar usuario:\n"+Back.RESET)
+                print(Fore.YELLOW+"Eliminar usuario:\n"+Fore.RESET)
                 id_usuario = int(input("Ingrese el ID del usuario a eliminar: ")).strip()
                 if not id_usuario or id_usuario <= 0 or id_usuario == "" or id_usuario is None:
                     print(Fore.RED + "Error: El ID del usuario es obligatorio." + Fore.RESET)
@@ -182,10 +182,10 @@ def submenu():
             ver_menu()
             
         elif seleccion == '7':
-            print(Fore.GREEN + "Saliendo del sistema..."+Fore.RESET)
+            print(Fore.YELLOW + "Saliendo del sistema..."+Fore.RESET)
             time.sleep(1)
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(Fore.GREEN + "Gregory Rodriguez © 2025\nDNI 95777596\nGracias por usar la aplicación."+Fore.RESET)
+            print(Fore.YELLOW + Style.BRIGHT+ "Gregory Rodriguez © 2025\nDNI 95777596\nGracias por usar la aplicación."+Fore.RESET+Style.RESET_ALL)
             time.sleep(1)
             os.system('cls' if os.name == 'nt' else 'clear')
         else:
